4111125d259cb23bb2e6972e345caff22247ff52
cruzmaria444 <cruzmaria4411@gmail.com>
2022-01-24 22:38:15 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-const userCtrl = require('./user/user');
-
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
